68db50c48188e617a3b13a87fa51adf9f62f01a4
David <alv.ricardodavid@gmail.com>
2024-04-27 20:40:09 -0300

    first commit

---
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -0,0 +1,24 @@
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'shared-search-box',
+  templateUrl: './search-box.component.html',
+  styles: [``]
+})
+export class SearchBoxComponent {
+
+  @Input()
+  public placeholder: string = ''
+
+  @Output()
+  public onValue: EventEmitter<string> = new EventEmitter<string>();
+
+  // @ViewChild('txtInput')
+  // public inputText!: ElementRef<HTMLInputElement>
+
+  emitValue(value: string): void {
+    // const textInput = this.inputText.nativeElement.value;
+    //console.log({'Search-Text:': value});
+    this.onValue.emit( value );
+  }
+}
